498f704087a57ce8fc9c23047480dab69029afa9
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2021-01-05 22:58:10 +0000

    Update whatis command

--- radia/cogs/info.py
+++ radia/cogs/info.py
@@ -49,13 +49,14 @@ class Info(commands.Cog):
         if prefix:
             try:
                 name, response, image_link = google.connector.whatis.get(prefix.lower())
-                embed = utils.Embed(title=f"What Is... {name.capitalize()}?", description=response)
-                if image:
-                    embed.set_image(image_link)
-                await ctx.send(embed=embed)
+                print("poop")
             except TypeError:
                 await ctx.send(self.invalid_whatis(prefix))
-
+            else:
+                embed = utils.Embed(title=f"What Is... {name.capitalize()}?", description=response)
+                if image:
+                    embed.set_image(url=image_link)
+                await ctx.send(embed=embed)
         else:
             embed = utils.Embed(title="What Is...")
             embed.add_field(
